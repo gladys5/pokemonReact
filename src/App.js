24141a939c7df70da62cff180d6c833816d1159b
gladys5 <yumanji@live.com>
2022-01-24 21:49:26 -0700

    pokemmit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import './App.css';import {Routes,Route} from "react-router-dom"
+import PokemonList from "./component/PokemonList"
+import Pokemon from "./component/Pokemon"
+import {NavLink} from "react-router-dom"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <nav>
+       <NavLink to={"/pokemon/test"}>Search</NavLink> 
+     </nav>
+     
+     <Routes>
+         <Route  path="/"  element={<PokemonList/>} />
+         <Route  path="pokemon/"  element={<Pokemon/>} />
+         
+        
+
+
+     </Routes>
+     
+     
+     
     </div>
   );
 }
---
+++ src/reducer/RootReducer.js
@@ -0,0 +1,14 @@
+
+
+
+import {combineReducers} from "redux";
+import PokemonListReducer from "./PokemonListReducer";
+import PokemonMultipleReducer from "./PokemonMultipleReducer";
+
+const RootReducer = combineReducers({
+  PokemonList: PokemonListReducer,
+  Pokemon: PokemonMultipleReducer
+});
+
+export default RootReducer;
+
